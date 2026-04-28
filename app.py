b18ee376fcbbec25c21eaccd50735ed2385ace01
moscatelglop-code <moscatel.glop@gmail.com>
2026-04-28 21:59:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def load_data():
     try:
         # Cargamos el CSV saltando la primera fila vacía
         df = pd.read_csv(
-            'CATALOGO 2026 GLOP.xlsx - Hoja1.csv', 
+            'CATALOGO 2026 GLOP.xlsx', 
             skiprows=1, 
             encoding='latin1', 
             on_bad_lines='skip', 
